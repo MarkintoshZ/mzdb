01344e2b4872d65ca60931473486156c6e5f8978
Mark Zhang <bf-109g2@hotmail.com>
2023-08-12 15:30:56 -0400

    Parse cmd args & Basic datatypes setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+use std::net::SocketAddr;
+
+use clap::Parser;
 use tonic::{transport::Server, Request, Response, Status};
 
 use mzdb::node_server::{Node, NodeServer};
@@ -5,12 +9,57 @@ use mzdb::{
     get_response, GetRequest, GetResponse, LookupRequest, LookupResponse, SetRequest, SetResponse,
 };
 
+/// MZBD server
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// ip address of the node
+    addr: SocketAddr,
+
+    /// ip address of the successor node
+    successor: SocketAddr,
+
+    /// node number
+    number: u64,
+
+    /// 2^k number hash slots
+    #[arg(default_value_t = 8)]
+    k: u64,
+}
+
+#[derive(Debug)]
+struct NodeInfo {
+    number: u64,
+    addr: SocketAddr,
+}
+
+#[derive(Debug, Default)]
+struct Storage {
+    table: HashMap<String, String>,
+}
+
+#[derive(Debug)]
+struct NodeState {
+    successor: SocketAddr,
+    fingers: Vec<NodeInfo>,
+    sockets: HashMap<i64, String>,
+    storage: Storage,
+}
+
 pub mod mzdb {
     tonic::include_proto!("mzdb");
 }
 
-#[derive(Debug, Default)]
-pub struct DBNode {}
+#[derive(Debug)]
+pub struct DBNode {
+    state: NodeState,
+}
+
+impl DBNode {
+    fn new(state: NodeState) -> Self {
+        Self { state }
+    }
+}
 
 #[tonic::async_trait]
 impl Node for DBNode {
@@ -49,12 +98,21 @@ impl Node for DBNode {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let greeter = DBNode::default();
+    // parse cmd args
+    let args = Args::parse();
+    let node_state = NodeState {
+        successor: args.successor,
+        fingers: Vec::new(),
+        sockets: HashMap::default(),
+        storage: Storage::default(),
+    };
+
+    // start gRPC server
+    let node = DBNode::new(node_state);
 
     Server::builder()
-        .add_service(NodeServer::new(greeter))
-        .serve(addr)
+        .add_service(NodeServer::new(node))
+        .serve(args.addr)
         .await?;
 
     Ok(())
